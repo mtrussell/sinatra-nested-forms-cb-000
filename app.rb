c1d422153a914e3b82cec1f31864193deddddac6
Matt Trussell <smtrussell@gmail.com>
2018-04-29 07:13:34 +0000

    Done.

--- app.rb
+++ app.rb
@@ -5,5 +5,25 @@ module FormsLab
 
     # code other routes/actions here
 
+    get '/' do
+      erb :root
+    end
+
+    get '/new' do
+      erb :'pirates/new'
+    end
+
+    post '/pirates' do
+      @pirate = Pirate.new(params[:pirate])
+
+      @ships = []
+      params[:pirate][:ships].each do |details|
+         ship = Ship.new(details)
+         @ships << ship
+      end
+
+      erb :'pirates/show'
+    end
+
   end
 end
