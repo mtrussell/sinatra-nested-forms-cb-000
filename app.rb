684ef6b4f69964f8f8e9f4cc488409b100e76f67
Matt Trussell <smtrussell@gmail.com>
2018-04-29 06:48:35 +0000

    automatically backed up by learn

--- app.rb
+++ app.rb
@@ -14,7 +14,8 @@ module FormsLab
     end
 
     post '/pirates' do
-      @pirate = params[:pirate]
+      @pirate = Pirate.new(params[:pirate])
+      
       params[:pirate][:ships].each do |details|
         Ship.new(details)
       end
